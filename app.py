4dcd9e4462f936303d789244f1e5bb20d0488407
Tianyu Yao <yaotianyu@berkeley.edu>
2022-12-06 22:09:44 -0500

    support photo colorization

--- app.py
+++ app.py
@@ -4,6 +4,19 @@ from werkzeug.security import generate_password_hash, check_password_hash
 import os
 import base64
 
+import io
+import json
+
+from torchvision import models
+import torch
+from torchvision import datasets
+import torchvision.transforms as transforms
+from skimage.color import lab2rgb, rgb2lab, rgb2gray
+import numpy as np
+from PIL import Image
+from flask import Flask, render_template, send_file, request, redirect
+from model.model import ColorizationNet, GrayscaleImageFolder
+
 app = Flask(__name__)
 app.secret_key = base64.b64encode(os.urandom(24)).decode('utf-8')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
@@ -57,7 +70,39 @@ def handle_signup():
 
 @app.route('/home')
 def home():
-    return render_template('home.html')s
+    return render_template('home.html')
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/image")
+def image():
+    return render_template("image.html")
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        file = request.files['file']
+        img_bytes = file.read()
+        image = get_prediction(image_bytes=img_bytes) # np array
+        return image
+
+@app.route('/image', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files.get('file')
+        if not file:
+            return
+        img_bytes = file.read()
+        img = get_prediction(image_bytes=img_bytes)
+        img = Image.fromarray((img*255).astype('uint8'))
+        img.save(os.path.join(app.root_path, 'static/result.jpg'))
+
+        return render_template('result.html')
+    return render_template('image.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
